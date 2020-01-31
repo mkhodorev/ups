f3b28f0a9207725bcc028ab09a99cd9f46ee804c
Maksim Khodorev <maksim.khodorev@rcntec.com>
2020-02-01 01:59:33 +0300

    added logging

--- safe-shutdown.c
+++ safe-shutdown.c
@@ -2,49 +2,65 @@
 #include <linux/reboot.h>
 #include <sys/reboot.h>
 #include <wiringPi.h>
+#include <syslog.h>
  
 #define VCC_STATUS_GPIO_PIN      22
 #define DEFAULT_SHUTDOWN_TIMEOUT 300
 
-void setupGPIO()
+void setupGPIO(void)
 {
   wiringPiSetup();
   pinMode(VCC_STATUS_GPIO_PIN, INPUT);
 }
 
-int isBattery()
+int isBattery(void)
 {
   return digitalRead(VCC_STATUS_GPIO_PIN);
 }
 
-void powerOff()
+void powerOff(void)
 {
+  syslog(LOG_INFO, "Power off.");
+  closelog();
   sync();
   system("poweroff");
 }
 
-int shutdownTimeout() {
+int readShutdownTimeout(void) {
   int waitBeforeShutdown = DEFAULT_SHUTDOWN_TIMEOUT;
   char* waitBeforeShutdownString = getenv("WAIT_BEFORE_SHUTDOWN");
-  if(waitBeforeShutdownString != NULL)
+  if (waitBeforeShutdownString != NULL)
   {
     waitBeforeShutdown = atoi(waitBeforeShutdownString);
-    if(waitBeforeShutdown == 0)
+    if (waitBeforeShutdown == 0)
       waitBeforeShutdown = DEFAULT_SHUTDOWN_TIMEOUT;
   }
   return waitBeforeShutdown;
 }
 
-int main(void)
-{
+int main(void) {
+  openlog("slog", LOG_PID | LOG_CONS, LOG_USER);
   setupGPIO();
-  int timeToReboot = shutdownTimeout();
-  while(timeToReboot > 0)
+  const int shutdownTimeout = readShutdownTimeout();
+  int timeToPowerOff = shutdownTimeout;
+  syslog(LOG_INFO, "Power control started. Battery run time is %d seconds.", shutdownTimeout);
+  while (timeToPowerOff > 0)
   {
-    if(isBattery())
-      timeToReboot--;
-    else
-      timeToReboot = shutdownTimeout();
+    if (isBattery()) {
+      if (timeToPowerOff == shutdownTimeout) {
+        syslog(LOG_INFO, "Battery power. The system will be powered off after %d seconds.", shutdownTimeout);
+      }
+      if (timeToPowerOff % 10 == 0) {
+        syslog(LOG_INFO, "Power off after %d sec...", timeToPowerOff);
+      }
+      timeToPowerOff--;
+    }
+    else {
+      if (timeToPowerOff < shutdownTimeout) {
+        syslog(LOG_INFO, "Power Supply has been restored.");
+      }
+      timeToPowerOff = shutdownTimeout;
+    }
     sleep(1);
   }
   powerOff();
